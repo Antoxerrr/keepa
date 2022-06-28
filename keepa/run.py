088f6f1e8f53d3c58929c38295127f76430fc2da
Antoxerrr <a.malyarov99@gmail.com>
2022-06-28 17:19:40 +1000

    Фикс импортов

--- keepa/run.py
+++ keepa/run.py
@@ -5,12 +5,10 @@ from pathlib import Path
 
 import boto3
 
-from keepa.config import get_config
-from keepa.extensions import DateStorageHandler
-from keepa.loader import Loader
+from config import get_config
+from extensions import DateStorageHandler
+from loader import Loader
 
-CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
-sys.path.insert(0, os.path.dirname(CURRENT_DIR))
 
 SOURCES_DIR = Path(__file__).resolve().parent
 PROJECT_DIR = SOURCES_DIR.parent
@@ -23,18 +21,18 @@ logging.basicConfig(
     format='%(levelname)s: %(asctime)s %(message)s', level=logging.INFO,
     filename=os.path.join(PROJECT_DIR, 'logs.log'), encoding='utf-8'
 )
-
+# s3.list_objects(Bucket='academydb')['Contents']
 if __name__ == '__main__':
     logging.info('Запущена отправка в хранилище')
 
     entries = CONFIG['entries']
 
-    # Грузим файлы в storage
-    loader = Loader(s3, entries)
-    loader.upload()
-
-    # По необходимости обрабатываем хранение по датам
-    for entry in entries:
-        if entry.get('date_handling', False):
-            dsh = DateStorageHandler(s3, entry.get('bucket_name'))
-            dsh.handle()
+    # # Грузим файлы в storage
+    # loader = Loader(s3, entries)
+    # loader.upload()
+    #
+    # # По необходимости обрабатываем хранение по датам
+    # for entry in entries:
+    #     if entry.get('date_handling', False):
+    #         dsh = DateStorageHandler(s3, entry.get('bucket_name'))
+    #         dsh.handle()
